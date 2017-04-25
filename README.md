5328b5841fab7f0307f97d371c3f10f561180ce9
sime1 <sime.pizzi@gmail.com>
2017-04-25 17:01:15 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# atavi-client-bundle-application
+# bundle_application_package
+
+> grunt plugin for bundling the files that make an application into one package
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install bundle_application_package --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('bundle_application_package');
+```
+
+## The "bundle_application_package" task
+
+### Overview
+WIP
+## Release History
+0.0.1: Initial release
+_(Nothing yet)_
---
+++ tasks/bundle_application_package.js
@@ -0,0 +1,54 @@
+/*
+ * atavi-client-bundle-application
+ *
+ *
+ * Copyright (c) 2017 sime1
+ * Licensed under the MIT license.
+ */
+
+var path = require('path');
+module.exports = function(grunt){
+
+  grunt.task.registerMultiTask('atavi-client-bundle-application', 'task to create the application package file', function(){
+    let src = this.data.src;
+    console.log(src);
+      //console.log(src[i]);
+      let out = this.data.out + '/' + this.data.name + '.json'; //nome del file di output
+
+      let cur = grunt.file.readJSON(src);
+      let dir = path.dirname(src) + '/';  //path della cartella
+      console.log("dir", dir);
+      let pkg = 
+      {
+        setup:'',
+        libs:[],
+        ui:''
+      };
+      //setup
+      console.log('setup', cur.setup);
+      if(cur.setup)
+      {
+        for(var j in cur.setup)
+        {
+          let setup = grunt.file.expand(dir + cur.setup[j]);
+          console.log(setup);
+          setup.forEach((file) => {pkg.setup += grunt.file.read(file).replace(/.*require\(.*\);?/g, '').replace(/module.exports.*/g, '');});  //require e exports non sono supportati dal browser
+        }
+      }
+      else
+        pkg.setup = grunt.file.read(dir + "/setup.js").replace(/.*require\(.*\);?/g, '').replace(/module.exports.*/g, '');
+      //libs
+      if(cur.libs)
+        cur.libs.forEach((lib) => {pkg.libs.push(lib);} );
+      else if(grunt.file.exists(dir + '/libs.json'))
+        pkg.libs = grunt.file.readJSON(dir + '/libs.json');
+      //UI
+      if(cur.ui)
+        pkg.ui = grunt.file.read(dir + '/' + cur.ui);
+        
+      var str = JSON.stringify(pkg, null, 2);
+      //str = str.replace(/\\n/g, '');
+      console.log(out);
+      grunt.file.write(out, str);
+  });
+}
